a9e336737f83ffdc0c6a466a0d055438f9fe505d
Julius <julius.klotz@posteo.de>
2021-05-19 21:31:58 +0200

    Added regex filterin and min size for find utility

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -7,9 +8,18 @@ struct CLI {
     dirs: Vec<String>,
     #[structopt(short, long = "--patterns", help = "List of patterns to use")]
     patterns: Vec<String>,
-    #[structopt(short, long = "--output", help = "Write results to an ouput file instead of stdout")]
+    #[structopt(
+        short,
+        long = "--output",
+        help = "Write results to an ouput file instead of stdout"
+    )]
     output: Option<String>,
-    #[structopt(short, long = "--size", help = "Match files above size", default_value = "0")]
+    #[structopt(
+        short,
+        long = "--size",
+        help = "Match files above size",
+        default_value = "0"
+    )]
     size: usize,
 }
 
@@ -20,51 +30,80 @@ pub struct MyFile {
 }
 
 impl MyFile {
-    pub fn new(name: String, path: String) -> Self {
+    pub fn new(name: String, path: String, size: u64) -> Self {
         Self {
             name,
             dir_in: path,
-            size_bytes: 0,
+            size_bytes: size,
         }
     }
 }
 
-fn main() {
-   let args = CLI::from_args(); 
-   let files = files::read_files(&args.dirs);
-   for file in files {
-       println!("{:?}", file.name);
-   }
+fn main() -> Result<(), std::ffi::OsString> {
+    let args = CLI::from_args();
+    let mut files = files::read_files(&args.dirs)?;
+    let mut patterns: Vec<Regex> = Vec::new();
+    for pattern in &args.patterns {
+        if let Ok(reg) = Regex::new(pattern) {
+            patterns.push(reg);
+        }
+    }
+    if patterns.len() > 0 {
+        filter::filter_files(&mut files, patterns, &args.size);
+    }
+    for file in &files {
+        println!("{:?}, {:?} KB", file.dir_in,  file.size_bytes);
+    }
+    Ok(())
+}
+
+#[derive(Debug, thiserror::Error)]
+enum Error {
+    #[error(transparent)]
+    CrosstermError(#[from] std::io::Error),
 }
 
 mod files {
     use super::MyFile;
     use std::path::Path;
+
     use std::fs::{self, DirEntry};
-    pub fn read_files(dirs: &Vec<String>) -> Vec<MyFile>{
+    pub fn read_files(dirs: &Vec<String>) -> Result<Vec<MyFile>, std::ffi::OsString> {
         let mut result: Vec<MyFile> = Vec::new();
         let mut dir_entries: Vec<DirEntry> = Vec::new();
         for dir in dirs {
-            let _ = scan_dir(Path::new(dir), & mut dir_entries);
+            let _ = scan_dir(Path::new(dir), &mut dir_entries);
         }
         for file in dir_entries {
-            result.push(MyFile::new(file.file_name().into_string().unwrap(), file.path().into_os_string().into_string().unwrap()));
+            let name = file.file_name().into_string()?;
+            let path = file.path().into_os_string().into_string()?;
+            let size = file.metadata().unwrap().len();
+            result.push(MyFile::new(name, path, size));
         }
-        result
+        Ok(result)
     }
 
-    fn scan_dir(dir: &Path, files: & mut Vec<DirEntry>) {
+    fn scan_dir(dir: &Path, files: &mut Vec<DirEntry>) -> Result<(), std::io::Error> {
         if dir.is_dir() {
-            for entry in fs::read_dir(dir).unwrap() {
+            for entry in fs::read_dir(dir)? {
                 let entry = entry.unwrap();
                 let path = entry.path();
                 if path.is_dir() {
-                    scan_dir(&path, files);
+                    let _ = scan_dir(&path, files);
                 } else {
-                   files.push(entry);
+                    files.push(entry);
                 }
             }
-        } 
-
+        }
+        Ok(())
     }
-} 
+}
+
+mod filter {
+    use super::MyFile;
+    pub fn filter_files(files: &mut Vec<MyFile>, patterns: Vec<regex::Regex>, min_size: &usize) {
+        for pattern in patterns {
+            files.retain(|file| pattern.is_match(&file.name) && file.size_bytes as usize > *min_size);
+        }
+    }
+}
